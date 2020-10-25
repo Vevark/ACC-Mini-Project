50d98a65171908bdc7f267bb36cc79c0559a5eaa
Vevark <754592699@qq.com>
2020-10-25 23:01:32 +0000

    update codes

--- blackbox/steps.sh
+++ blackbox/steps.sh
@@ -7,13 +7,17 @@ tar xzvf murtazo.tgz
 cd murtazo
 tar xvf cloudnaca.tgz
 tar xvf navier_stokes_solver.tar
+
 cd navier_stokes_solver/src
 ./compile_forms
 cd ..
 cmake .
 make -j 2
+
 cd ../cloudnaca
 sed -i 's|/Applications/Gmsh.app/Contents/MacOS/gmsh|/usr/bin/gmsh|g' runme.sh
 ./runme.sh 0 30 10 200 3
-cd msh
+
+cd ..
+cd ..
 python3 convert.py
