6b53eb02f3c22f0e41fb2a8235c2dc78372e4c03
Vevark <754592699@qq.com>
2020-10-25 18:20:35 +0000

    update codes

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+apt install docker-compose
